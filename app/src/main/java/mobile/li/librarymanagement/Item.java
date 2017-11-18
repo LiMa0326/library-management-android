82ecdb0b7daa0abf333dbcafa5252d9d7fdb2848
Li Ma <scontdiplex@outlook.com>
2017-11-17 17:25:45 -0800

    Initial commit

---
+++ app/src/main/java/mobile/li/librarymanagement/Item.java
@@ -0,0 +1,23 @@
+package mobile.li.librarymanagement;
+
+/**
+ * Created by Li on 2017/11/17.
+ */
+
+public class Item {
+    private String title;
+
+    public Item() {}
+
+    public Item(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
