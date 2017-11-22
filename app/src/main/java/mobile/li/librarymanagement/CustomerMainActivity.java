c54f349b17bf88e83a3cc388b1c9e2cf267a7628
Li Ma <scontdiplex@outlook.com>
2017-11-21 17:17:46 -0800

    Modify activity main for separate librarian and customer. Add another 2 activities.

--- app/src/main/java/mobile/li/librarymanagement/CustomerMainActivity.java
+++ app/src/main/java/mobile/li/librarymanagement/CustomerMainActivity.java
@@ -1,51 +1,52 @@
 package mobile.li.librarymanagement;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-
-public class MainActivity extends AppCompatActivity {
+public class CustomerMainActivity extends AppCompatActivity {
 
     private FirebaseAuth mFirebaseAuth;
     private FirebaseUser mFirebaseUser;
+    private DatabaseReference mDatabase;
+    private String mLibrarianId;
     private String mLibrarianEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        // Initialize Firebase Auth and Database Reference
-        mFirebaseAuth = FirebaseAuth.getInstance();
-        mFirebaseUser = mFirebaseAuth.getCurrentUser();
-
-
-        if (mFirebaseUser == null){
-            // Not logged in, launch the Log In activity
-            loadLogInView();
-        } else {
-            mLibrarianEmail = mFirebaseUser.getEmail();
-            if(!mLibrarianEmail.contains("@sjsu.edu")){
-                Intent customerIntent = new Intent(this, CustomerMainActivity.class);
-                customerIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                customerIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                startActivity(customerIntent);
-            }else{
-                Intent libIntent = new Intent(this, LibrarianMainActivity.class);
-                libIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                libIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                startActivity(libIntent);
-            }
-        }
+        setContentView(R.layout.activity_customer_main);
+//
+//        // Initialize Firebase Auth and Database Reference
+//        mFirebaseAuth = FirebaseAuth.getInstance();
+//        mFirebaseUser = mFirebaseAuth.getCurrentUser();
+//        mDatabase = FirebaseDatabase.getInstance().getReference();
+//
+//        if (mFirebaseUser == null){
+//            // Not logged in, launch the Log In activity
+//            loadLogInView();
+//        } else {
+//            mLibrarianId = mFirebaseUser.getUid();
+//            mLibrarianEmail = mFirebaseUser.getEmail();
+//        }
     }
 
     private void loadLogInView() {
@@ -71,7 +72,7 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_logout) {
-            mFirebaseAuth.signOut();
+            //mFirebaseAuth.signOut();
             loadLogInView();
         }
 
